d8bfeb56c437c542de7b08672572c62c4329f13a
Rully-Rem84 <rsatriawan@gmail.com>
2025-04-09 00:04:43 +0700

    change in main

--- Main.py
+++ Main.py
@@ -2,8 +2,12 @@ print('Ibu memberi perintah: "Beli 1 botol susu"')
 print('Budi menjawab: "OK"')
 print('kemudian Budi pergi ke toko')
 
-milk_count = 2
-egg_count = 7
+milk_count = 5
+egg_count = 4
+
+print(f'jumlah botol susu {milk_count} botol')
+print(f'jumlah telur {egg_count} butir')
+
 
 if milk_count <1:
     print('Budi tidak jadi membeli susu')
@@ -11,5 +15,7 @@ else:
     print('Budi mendapat susu kemudian mengecek adakah telur?')
     if egg_count >6:
         print('Budi membeli 1 botol susu dan 6 butir telur')
+    else:
+        print('Budi Hanya membeli susu')
 
 print('Budi pulang dan melaporkan hasil belanja kepada ibu')
