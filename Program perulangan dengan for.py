20ea1a1ac960966db86f3902f33b2eb5f327d6a5
Rully-Rem84 <rsatriawan@gmail.com>
2025-04-10 11:15:04 +0700

    Penambahan program perulangan menggunakan for dan while

---
+++ Program perulangan dengan for.py
@@ -0,0 +1,19 @@
+'''
+PROGRAM PERULANGAN MENGGUNAKAN FOR
+'''
+
+
+print ("Mr.X meminta membaca semua buku")
+
+jumlah_buku = 56
+print( f"jumlah buku: {jumlah_buku}")
+buku_telah_dibaca = 0
+print(f"buku yang telah dibaca: {buku_telah_dibaca}")
+
+for i in range(1, jumlah_buku+1):
+    print(f"Buku ke {i} sudah dibaca")
+
+print("Seluruh buku sudah dibaca")
+
+
+
---
+++ Program perulangan dengan while.py
@@ -0,0 +1,15 @@
+'''
+PROGRAM PERULANGAN MENGGUNAKAN WHILE
+'''
+
+print ("Mr.X meminta membaca semua buku")
+jumlah_buku = 15
+buku_telah_dibaca = 5
+print(f"Jumlah Buku: {jumlah_buku}")
+print(f"Buku yang telah dibaca: {buku_telah_dibaca}")
+
+while buku_telah_dibaca < jumlah_buku:
+    buku_telah_dibaca = buku_telah_dibaca + 1
+    print(f"Buku ke {buku_telah_dibaca} telah dibaca")
+
+print("Seluruh buku telah dibaca")
