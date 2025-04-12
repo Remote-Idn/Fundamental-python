e836849b233a06b84aadc3a3ffb3a9a362248cc5
Rully-Rem84 <rsatriawan@gmail.com>
2025-04-12 20:43:04 +0700

    Belajar list di python

--- Belajar List.py
+++ Belajar List.py
@@ -2,17 +2,39 @@
 Belajar Menggunakan Data List Di Python
 '''
 import math
+print("Nilai Ujian Siswa Kelas 4")
+
 Nilai_Ujian = ["45", "75", "80", "85", "90", "95", "75", "100", "75",
                "65"]
-print(f"Jumlah Siswa adalah: {(len(Nilai_Ujian))}")
+for i in range(0, len(Nilai_Ujian)):
+    print(Nilai_Ujian[i])
+
+Nilai_Ujian.pop(2)
+for i in range (0, len(Nilai_Ujian)):
+    print(Nilai_Ujian[i])
+
+print(f"\nJumlah Siswa adalah: {(len(Nilai_Ujian))}")
 print(f"Nilai siswa ke-3 adalah: {Nilai_Ujian[2]}")
 Nilai_Ujian.append ("50")
 
 print(Nilai_Ujian)
 print(len(Nilai_Ujian))
-if Nilai_Ujian[10] < "50":
+if Nilai_Ujian[8] < "50":
     print("Tidak Lulus")
 else:
     print("Lulus")
 
 print(Nilai_Ujian[4])
+
+print("\nBELANJA BUAH")
+Buah = ["Jeruk", "Apel", "Nanas", "Sirsat", "Mangga"]
+for i in range (0, len(Buah)):
+    print(Buah[i])
+Buah.append("Pepaya")
+for i in range (0, len(Buah)):
+    print(Buah[i])
+
+for i in range (0, len(Buah)):
+    print(Buah[i])
+print(f"\nJumlah Jenis Buah Yang Dibeli: {len(Buah)} Jenis")
+
